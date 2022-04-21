0c03ba884b6d50880c12fa24b5ab29085d432b88
PDXing <y1143913447@qq.com>
2022-04-21 16:21:22 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
 
-createApp(App).use(router).use(store).mount('#app')
+import * as ElIconModules from '@element-plus/icons'
+
+const app = createApp(App)
+
+for (const iconName in ElIconModules) {
+  if (Reflect.has(ElIconModules, iconName)) {
+    const item = ElIconModules[iconName]
+    app.component(iconName, item)
+  }
+}
+
+app.use(router).use(store).mount('#app')
